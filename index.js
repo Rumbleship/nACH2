a3425f3ab14e21cb570f2053cef8e2ad9068d715
Mathew Henshall <mat.henshall@rumbleship.com>
2018-10-02 17:35:29 -0700

    export EntryAddenda

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports.Entry = require('./lib/entry');
+module.exports.EntryAddenda = require('./lib/entry-addenda');
 module.exports.Batch = require('./lib/batch');
-module.exports.File  = require('./lib/file');
-module.exports.Utils  = require('./lib/utils');
-module.exports.Validate  = require('./lib/validate');
+module.exports.File = require('./lib/file');
+module.exports.Utils = require('./lib/utils');
+module.exports.Validate = require('./lib/validate');
